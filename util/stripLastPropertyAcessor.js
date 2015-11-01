29befa5bac3c4d8c19dc885ee2bd215c3d4fa4de
Jiří Špác <jiri.spac.external@socialbakers.com>
2015-11-01 03:02:27 +0100

    use babel format string even in the util file

--- util/stripLastPropertyAcessor.js
+++ util/stripLastPropertyAcessor.js
@@ -1,8 +1,8 @@
-export default function stripLastPropertyAcessor(path){
+'use babel'
+export default function stripLastPropertyAcessor (path) {
 	if (path[path.length - 1] === ']') {
-		return path.substring(0, path.lastIndexOf('['));
+		return path.substring(0, path.lastIndexOf('['))
 	} else {
-		return path.substring(0, path.lastIndexOf('.'));
+		return path.substring(0, path.lastIndexOf('.'))
 	}
-
-};
+}
